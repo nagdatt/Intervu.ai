de1514f99878c0111b48dbbfc9a41e368154b035
Nagdatt <59867066+nagdatt@users.noreply.github.com>
2025-12-01 15:55:54 +0530

    added db connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,29 +1,42 @@
 import express from 'express';
-import {ENV} from './lib/env.js';
+import { ENV } from './lib/env.js';
 import path from 'path';
-const app=express();
-const __dirname=path.resolve();
+import { connectDB } from './lib/db.js';
+const app = express();
+const __dirname = path.resolve();
 
 
-app.get('/health',(req,res)=>{
+app.get('/health', (req, res) => {
     res.status(200).json({
-        message:'api is up and running'
+        message: 'api is up and running'
     });
 });
-app.get('/books',(req,res)=>{
+app.get('/books', (req, res) => {
     res.status(200).json({
-        message:'books route'
+        message: 'books route'
     });
 });
 
 
 // make our app deployement ready
-if(ENV.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'../frontend/dist')));
-    app.get("/{*any}",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+if (ENV.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
     })
 }
-app.listen(ENV.PORT,()=>{
-    console.log('Server is running on port 3000');
-});
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        app.listen(ENV.PORT, () => {
+            console.log('Server is running on port 3000');
+        });
+    }
+    catch (error) {
+        console.error('Failed to start server', error);
+        process.exit(1);
+    }
+}
+startServer();
