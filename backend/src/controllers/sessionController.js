897a31a821f08f611ad8e2dfd87cc3d6bbb61a11
Nagdatt <59867066+nagdatt@users.noreply.github.com>
2025-12-04 19:48:02 +0530

    updated session controller

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -103,6 +103,16 @@ try {
     if(session.participant){
         return res.status(404).json({message:"Session is already full"});
     }
+//host cannot join as participant
+    if(session.host.toString()===userId.toString()){
+        return res.status(400).json({message:"Host cannot join as participant"});
+    }
+    //check if session is completed
+    if(session.status!=="active"){
+        return res.status(400).json({message:"Cannot join a completed session"});
+    }
+
+
     session.participant=userId;
     await session.save();
     //add participant to stream video call
