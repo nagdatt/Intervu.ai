6a26750e74aeca80240a061240677cc777bb8ca7
Nagdatt <59867066+nagdatt@users.noreply.github.com>
2025-12-04 23:31:46 +0530

    problem detail page added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,23 +5,26 @@ import HomePage from './pages/HomePage.jsx'
 import ProblemsPage from './pages/ProblemsPage.jsx'
 import { Toaster } from 'react-hot-toast'
 import DashBoardPage from './pages/DashBoardPage.jsx'
+import ProblemPage from './pages/ProblemPage.jsx'
 
 function App() {
- const {isSignedIn,isLoaded} = useUser()
-  if(!isLoaded){
+  const { isSignedIn, isLoaded } = useUser()
+  if (!isLoaded) {
     return null
   }
   return (
     <>
 
       <Routes>
-        <Route path='/' element={!isSignedIn?<HomePage />:<Navigate to={"/dashboard"}/>} />
-                <Route path='/dashboard' element={isSignedIn?<DashBoardPage />:<Navigate to={"/"}/>} />
+        <Route path='/' element={!isSignedIn ? <HomePage /> : <Navigate to={"/dashboard"} />} />
+        <Route path='/dashboard' element={isSignedIn ? <DashBoardPage /> : <Navigate to={"/"} />} />
 
-        <Route path='/problems' element={isSignedIn===true?<ProblemsPage />:<Navigate to={"/"}/>} />
+        <Route path='/problems' element={isSignedIn === true ? <ProblemsPage /> : <Navigate to={"/"} />} />
+        <Route path='/problem/:id' element={isSignedIn === true ? <ProblemPage /> : <Navigate to={"/"} />} />
+
 
       </Routes>
-    <Toaster toastOptions={{duration:3000}}/>
+      <Toaster toastOptions={{ duration: 3000 }} />
 
     </>
   )
